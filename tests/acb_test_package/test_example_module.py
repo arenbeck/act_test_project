613e5247f2b4aac3d5cfd4db2b69425c3783bc9a
arenbeck <ac.beck@ucf.edu>
2024-06-06 11:01:28 -0500

    Initial commit

---
+++ tests/acb_test_package/test_example_module.py
@@ -0,0 +1,13 @@
+from acb_test_package import example_module
+
+
+def test_greetings() -> None:
+    """Verify the output of the `greetings` function"""
+    output = example_module.greetings()
+    assert output == "Hello from LINCC-Frameworks!"
+
+
+def test_meaning() -> None:
+    """Verify the output of the `meaning` function"""
+    output = example_module.meaning()
+    assert output == 42
